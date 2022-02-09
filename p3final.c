7206dca71acea96cb2fb8f1f8d1b1e8edb76a5f2
aries <94883201+Aries417vi@users.noreply.github.com>
2022-02-09 11:07:18 +0000

    p1final

---
+++ p3final.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int input_number()
+{
+  int x;
+  printf("Enter the number:\n");
+  scanf("%d", &x);
+  return x;
+}
+
+int is_composite(int n)
+{
+  for (int i = 2; i <= n/2; i++)
+  {
+    if (n%i == 0)
+      return 1;
+  }
+  return 0;
+}
+
+void output(int n, int isComposite)
+{
+  if (isComposite)
+    printf("%d is a composite number\n", n);
+  else 
+    printf("%d is not a composite number\n", n);
+}
+
+int main()
+{
+  int x = input_number();
+  int res = is_composite(x);
+  output(x, res);
+  return 0;
+}
